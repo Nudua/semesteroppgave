e839843273a2634808c32d96a1097fe702871691
Tor Ramstad <tor.audun@gmail.com>
2018-02-27 11:08:01 +0100

    Changes to soundmanager

--- src/com/groupname/framework/audio/SoundManager.java
+++ src/com/groupname/framework/audio/SoundManager.java
@@ -6,16 +6,20 @@ import javafx.scene.media.AudioClip;
 import javax.sound.sampled.*;
 import java.io.*;
 import java.net.URL;
+import java.util.List;
 
 
 public class SoundManager {
 
- private final AudioClip Crash;
- private final AudioClip Hit;
- private final AudioClip BG;
+    private final AudioClip Crash;
+    private final AudioClip Hit;
+    private final AudioClip BG;
 
-private Clip clip;
-     SoundManager(String Sound) {
+    private Clip clip;
+
+    private List<AudioClip> audioClips;
+
+    public SoundManager(String Sound) {
 
         Crash= new AudioClip("file:///C:/Users/phamd/Documents/java/src/lyd/background.wav");
         Hit= new AudioClip("file:///C:/Users/phamd/Documents/java/src/lyd/background.wav");
@@ -34,6 +38,12 @@ private Clip clip;
             e.printStackTrace();
         }
 
+    }
+
+    public void addClip(String fileName) {
+        String directory = "";
+        AudioClip clip = new AudioClip(directory + fileName);
+        audioClips.add(clip);
     }
 
     public void play(final AudioClip clip){
