7fe77d82bed1f2c0012682ff496833833b246dff
Håkon Åreskjold <haareskjold@gmail.com>
2018-03-06 11:30:46 +0100

    Player test

--- src/com/groupname/game/entities/Player.java
+++ src/com/groupname/game/entities/Player.java
@@ -1,15 +1,19 @@
 package com.groupname.game.entities;
 import com.groupname.framework.graphics.Sprite;
 import com.groupname.framework.graphics.drawing.SpriteBatch;
+import com.groupname.framework.graphics.drawing.SpriteFlip;
 import com.groupname.framework.input.InputManager;
 import com.groupname.framework.input.devices.KeyboardInput;
 import com.groupname.framework.math.Vector2D;
 import com.groupname.game.entities.Actor;
 
+import java.util.EnumSet;
+
 public class Player extends Actor {
     private final InputManager inputManager;
     private final Vector2D initialPosition;
     private double speed = 8.5d;
+    private SpriteFlip spriteFlip = SpriteFlip.NONE;
 
     public Player(Sprite sprite, Vector2D position, int hitPoints, InputManager inputManager) {
         super(sprite, position, hitPoints);
@@ -36,8 +40,10 @@ public class Player extends Actor {
         double y = position.getY();
         if(inputManager.isDown(KeyboardInput.Defaults.LEFT)) {
             position.setX(x - speed);
+            spriteFlip = SpriteFlip.HORIZONTAL;
         } else if (inputManager.isDown((KeyboardInput.Defaults.RIGHT))) {
             position.setX(x + speed);
+            spriteFlip = SpriteFlip.NONE;
         }
         if(inputManager.isDown(KeyboardInput.Defaults.UP)) {
             position.setY(y - speed);
@@ -48,6 +54,7 @@ public class Player extends Actor {
 
     @Override
     public void draw(SpriteBatch spriteBatch) {
-        spriteBatch.draw(sprite,position);
+        spriteBatch.draw(sprite,position, EnumSet.of(spriteFlip));
+
     }
 }
