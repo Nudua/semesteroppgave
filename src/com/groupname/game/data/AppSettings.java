380655859e9e7ee108c9023b381224d112b8f7d3
Håkon Åreskjold <haareskjold@gmail.com>
2018-03-19 11:29:48 +0100

    AppSettings - start

---
+++ src/com/groupname/game/data/AppSettings.java
@@ -0,0 +1,25 @@
+package com.groupname.game.data;
+
+import com.groupname.framework.math.Size;
+
+class
+
+public enum AppSettings {
+    INSTANCE;
+
+    public static final Size SCREEN_BOUNDS = new Size(1200, 720);
+    private boolean fullScreen;
+    private String nickname;
+
+    public void save() {
+
+    }
+
+    public void load() {
+
+    }
+
+}
+//AppSettings.INSTANCE.save();
+//AppSettings.INSTANCE.load();
+//AppSettings.SCREEN_BOUNDS;
