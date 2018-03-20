de17bdd2984e76d5d1c193e947bc2189bed55b15
Tor Ramstad <tor.audun@gmail.com>
2018-03-20 14:15:23 +0100

    KeyboardInputTests updates

--- test/com/groupname/input/devices/KeyboadInputTests.java
+++ test/com/groupname/input/devices/KeyboadInputTests.java
@@ -1,14 +1,33 @@
 package com.groupname.input.devices;
 import com.groupname.framework.input.devices.KeyboardInput;
+import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class KeyboadInputTests {
 
-    Scene scene = new Scene(new GridPane());
+    // This is required because we're not allowed to create a scene unless it's on a javafx thread
+    private class FXTestApp extends Application {
+        private Scene scene = new Scene(new GridPane());
+
+        @Override
+        public void start(Stage primaryStage) {
+        }
+    }
+
+    private FXTestApp app = new FXTestApp();
+    private Scene scene;
+
+    @Before
+    public void before() {
+        FXTestApp app = new FXTestApp();
+        scene = app.scene;
+    }
 
     @Test(expected = NullPointerException.class)
     public void checkSceneNotNull() {
