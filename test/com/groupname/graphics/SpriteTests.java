20a8cb1f268739a68b73671ad653239a2b56ffc9
Tor Ramstad <tor.audun@gmail.com>
2018-03-20 12:06:20 +0100

    Unit tests for the Sprite class

--- test/com/groupname/graphics/SpriteTests.java
+++ test/com/groupname/graphics/SpriteTests.java
@@ -1,55 +1,145 @@
 package com.groupname.graphics;
 
 import com.groupname.framework.graphics.Sprite;
+import com.groupname.framework.graphics.SpriteSheet;
+import com.groupname.framework.io.Content;
+import com.groupname.framework.io.ResourceType;
+import com.groupname.framework.math.IntVector2D;
 import com.groupname.framework.util.EmptyStringException;
 import javafx.scene.shape.Rectangle;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class SpriteTests {
 
     private final String name = "sprite1";
-    private final String spriteSheet = "spritesheet1";
-    private final Rectangle testSpriteRegion = new Rectangle(64,64);
+    private final SpriteSheet spriteSheet = getValidSpriteSheet();
+    private final Rectangle spriteRegion = new Rectangle(64,64);
 
-    /*
+    // Helper methods
+    private SpriteSheet getValidSpriteSheet() {
+        return new SpriteSheet("testSheet", Content.loadImage("projectiles.png", ResourceType.SpriteSheet));
+    }
+
+    private Sprite getValidSprite() {
+        return new Sprite(name, spriteSheet, spriteRegion);
+    }
+
     // Constructor parameter tests
     @Test(expected = NullPointerException.class)
     public void NameParameterCannotBeNull() {
-        Sprite sprite = new Sprite(null, spriteSheet, testSpriteRegion);
+        new Sprite(null, spriteSheet, spriteRegion);
     }
 
     @Test(expected = EmptyStringException.class)
     public void NameParameterCannotBeEmpty() {
-        Sprite sprite = new Sprite("", spriteSheet, testSpriteRegion);
+        new Sprite("", spriteSheet, spriteRegion);
     }
 
     @Test(expected = NullPointerException.class)
     public void SpriteSheetParameterCannotBeNull() {
-        Sprite sprite = new Sprite(name, null, testSpriteRegion);
-    }
-
-    @Test(expected = EmptyStringException.class)
-    public void SpriteSheetParameterCannotBeEmpty() {
-        Sprite sprite = new Sprite(name,"", testSpriteRegion);
+        new Sprite(name, null, spriteRegion);
     }
 
     @Test(expected = NullPointerException.class)
     public void SpriteRegionParameterCannotBeNull() {
-        Sprite sprite = new Sprite(name, spriteSheet, null);
+        new Sprite(name, spriteSheet, null);
     }
 
-    // Methods
+    // Getters
     @Test
     public void ReturnsCorrectWidth() {
-        Sprite sprite = new Sprite(name, spriteSheet, testSpriteRegion);
-        assertTrue(sprite.getWidth() == testSpriteRegion.getWidth());
+        Sprite sprite = new Sprite(name, spriteSheet, spriteRegion);
+        assertEquals(sprite.getWidth(), spriteRegion.getWidth(), 0.0);
     }
 
     @Test
     public void ReturnsCorrectHeight() {
-        Sprite sprite = new Sprite(name, spriteSheet, testSpriteRegion);
-        assertTrue(sprite.getHeight() == testSpriteRegion.getHeight());
+        Sprite sprite = new Sprite(name, spriteSheet, spriteRegion);
+        assertEquals(sprite.getHeight(), spriteRegion.getHeight(), 0.0);
     }
-    */
+
+    @Test
+    public void setScaleAffectsWidthAndHeight() {
+        Sprite sprite = getValidSprite();
+        assertNotNull(sprite);
+
+        double originalWidth = sprite.getWidth();
+        double originalHeight = sprite.getHeight();
+
+        double scale = 3.0;
+
+        sprite.setScale(scale);
+
+        double newWidth = scale * originalWidth;
+        double newHeight = scale * originalHeight;
+
+        assertEquals(newWidth, sprite.getWidth(), 0.0d);
+        assertEquals(newHeight, sprite.getHeight(), 0.0d);
+    }
+
+    @Test
+    public void getNameGivesCorrectValue() {
+        Sprite sprite = getValidSprite();
+
+        assertEquals(sprite.getName(), name);
+    }
+
+    @Test
+    public void getSpriteSheetGivesCorrectValue() {
+        Sprite sprite = getValidSprite();
+
+        assertSame(sprite.getSpriteSheet(), spriteSheet);
+    }
+
+    @Test
+    public void getSpriteRegionGivesCorrectValue() {
+        Sprite sprite = getValidSprite();
+
+        assertSame(sprite.getSpriteRegion(), spriteRegion);
+    }
+
+    @Test
+    public void setSpriteRegionUpdatesSpriteRegion() {
+        Sprite sprite = getValidSprite();
+
+        assertSame(sprite.getSpriteRegion(), spriteRegion);
+
+        Rectangle newRegion = new Rectangle(0,0,200,400);
+        assertNotNull(newRegion);
+
+        sprite.setSpriteRegion(newRegion);
+
+        assertNotSame(sprite.getSpriteRegion(), spriteRegion);
+        assertSame(sprite.getSpriteRegion(), newRegion);
+    }
+
+    // Static methods
+    // Look into this one more
+    @Test
+    public void createSpriteRegionGivesCorrectRegion() {
+        int width = 200;
+        int height = 200;
+
+        int x = 23;
+        int y = 44;
+
+        Rectangle region = Sprite.createSpriteRegion(width, height);
+        validateRectangle(region, width, height, 0, 0);
+
+        Rectangle region2 = Sprite.createSpriteRegion(x, y, width, height);
+        validateRectangle(region2, width, height, x * width, y * height);
+
+        IntVector2D offset = new IntVector2D(20,40);
+        Rectangle region3 = Sprite.createSpriteRegion(x, y, width, height, offset);
+        validateRectangle(region3, width, height, x * width + (int)offset.getX(), y * height + (int)offset.getY());
+    }
+
+    private void validateRectangle(Rectangle region, int width, int height, int x, int y) {
+        assertEquals((int)region.getWidth(), width);
+        assertEquals((int)region.getHeight(), height);
+        assertEquals((int)region.getX(), x);
+        assertEquals((int)region.getY(), y);
+    }
+
 }
