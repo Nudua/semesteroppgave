a3b2a3928555eb4b4c2dc68ed989ce1f54f6606c
Håkon Åreskjold <haareskjold@gmail.com>
2018-04-24 13:40:07 +0200

    XMLWriter + Javadoc

--- src/com/groupname/framework/io/XMLMain.java
+++ src/com/groupname/framework/io/XMLMain.java
@@ -1,16 +1,35 @@
 package com.groupname.framework.io;
 
+import com.groupname.framework.math.Vector2D;
+import com.groupname.game.editor.metadata.EnemyMetaData;
+import com.groupname.game.editor.metadata.ObjectMetaData;
+import com.groupname.game.editor.metadata.PowerUpMetaData;
+import com.groupname.game.entities.SpriteFactory;
+import com.groupname.game.entities.enemies.GuardEnemy;
+import com.groupname.game.entities.enemies.HomingEnemy;
+import com.groupname.game.entities.enemies.TowerEnemy;
+import com.groupname.game.entities.powerups.HeartPowerUp;
 import javafx.scene.shape.Path;
 
 import java.nio.file.Paths;
 
 public class XMLMain {
 
-    public static void Main (String[] args) throws Exception {
+    public static void main (String[] args) throws Exception {
+        Content.setContentBaseFolder("/com/groupname/game/resources");
+
         XMLWriter xmlWriter = new XMLWriter();
 
         XMLNode node = new XMLNode("Tor", "Håkon");
 
-        xmlWriter.write(Paths.get("test.xml"), node);
+        //xmlWriter.write(Paths.get("test.xml"), node);
+        //xmlWriter.write(Paths.get("test.xml"), new Vector2D());
+        //xmlWriter.write(Paths.get("test.xml"), new SpriteFactory());
+        //xmlWriter.write(Paths.get("test.xml"), new ObjectMetaData("OSLOMET", GuardEnemy.class));
+        //xmlWriter.write(Paths.get("test.xml"), new PowerUpMetaData("Test", HeartPowerUp.class, 3));
+        xmlWriter.write(Paths.get("test.xml"), new EnemyMetaData("Test", HomingEnemy.class));
+
+
+
     }
 }
--- src/com/groupname/framework/io/XMLWriter.java
+++ src/com/groupname/framework/io/XMLWriter.java
@@ -1,13 +1,23 @@
 package com.groupname.framework.io;
 
+import com.groupname.framework.core.Difficulty;
+import com.groupname.game.editor.metadata.PowerupSpriteType;
+import com.groupname.game.entities.EnemySpriteType;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 class XMLNode {
     private final String name;
@@ -27,6 +37,11 @@ class XMLNode {
         return value;
     }
 
+    /**
+     * The string representation is XMLNode where name = name and value = value
+     * Where name is the name of the field you save, and value is the value of the field
+     * @return a print of the XMLNode.
+     */
     @Override
     public String toString() {
         return "XMLNode{" +
@@ -36,8 +51,21 @@ class XMLNode {
     }
 }
 
+/**
+ * XMLWriter takes an instance of a class and saves it to a file.
+ * Saves as XML to file.
+ */
 public class XMLWriter {
 
+    /**
+     * Reads the fields of the instance of the class.
+     * Saves it to "fileName" as XML.
+     * @param fileName filename to the file you save to.
+     * @param instance the instance of the class you want to store
+     * @param <T> the class type you want to store
+     * @throws IllegalAccessException todo
+     * @throws IOException todo
+     */
     public <T> void write(Path fileName, T instance) throws IllegalAccessException, IOException {
         List<XMLNode> nodes = readFields(fileName, instance);
         writeXML(fileName, instance.getClass().getName(), nodes);
@@ -48,52 +76,85 @@ public class XMLWriter {
 
         List<String> validClasses = new ArrayList<>();
 
+
         validClasses.add(String.class.getName());
+
         //Primitives
         validClasses.add(int.class.getName());
         validClasses.add(double.class.getName());
         validClasses.add(float.class.getName());
         validClasses.add(char.class.getName());
         validClasses.add(boolean.class.getName());
+        validClasses.add(Class.class.getTypeName());
+        validClasses.add(Difficulty.class.getName());
+        validClasses.add(EnemySpriteType.class.getName());
+        validClasses.add(PowerupSpriteType.class.getName());
 
-        Field[] fields = instance.getClass().getDeclaredFields();
+
+        List<Field> fields = getAllFields(instance);
         String className = instance.getClass().getName();
 
         List<XMLNode> nodes = new ArrayList<>();
 
         for(Field field : fields) {
-            if(!validClasses.contains(field.getType().getName())) {
+            if(Modifier.isStatic(field.getModifiers())) {
                 continue;
             }
-            boolean isAccessible = field.isAccessible();
+            if(validClasses.contains(field.getType().getName())) {
 
-            if(!isAccessible) {
-                field.setAccessible(true);
+                boolean isAccessible = field.isAccessible();
+
+                if(!isAccessible) {
+                    field.setAccessible(true);
+                }
+                String fieldValue = field.get(instance).toString();
+                if(field.getType().getName().equals(Class.class.getTypeName())) {
+                    fieldValue = fieldValue.replace("class ", "");
+                }
+
+                XMLNode node = new XMLNode(field.getName(), fieldValue);
+
+
+                if(!isAccessible) {
+                    field.setAccessible(false);
+                }
+
+                nodes.add(node);
             }
 
-            XMLNode node = new XMLNode(field.getName(), field.get(instance).toString());
 
-            if(!isAccessible) {
-                field.setAccessible(false);
-            }
 
-            nodes.add(node);
         }
         return nodes;
+    }
+
+    private <T> List<Field> getAllFields(T instance) {
+        List<Field> fields = new ArrayList<>();
+        Class objectOfClass = instance.getClass();
+        while(objectOfClass != Object.class){
+            fields.addAll(Arrays.asList(objectOfClass.getDeclaredFields()));
+            objectOfClass = objectOfClass.getSuperclass();
+        }
+        return fields;
     }
 
     private void writeXML(Path fileName, String className, List<XMLNode> nodes) throws IOException {
         final String openTag = "<";
         final String openEndTag = ">";
         final String closingEndTag = "</";
+        final String newLine = "\n";
+        final String tab = "\t";
 
-        try(BufferedWriter bufferedWriter = Files.newBufferedWriter(fileName, StandardOpenOption.WRITE, StandardOpenOption.CREATE)) {
+        try(BufferedWriter bufferedWriter = Files.newBufferedWriter(fileName, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE)) {
             //Writes the basenode in the following format: <classname>
-            bufferedWriter.write(openTag + className + openEndTag);
-
+            bufferedWriter.write(openTag + className + openEndTag + newLine);
             for(XMLNode node : nodes) {
-                bufferedWriter.write(openTag + node.getName() + openEndTag + node.getValue() + closingEndTag + node.getName() + openEndTag);
+                //Writes the field in the following format: <variableName>value</variableName>
+                bufferedWriter.write(tab + openTag + node.getName() + openEndTag + node.getValue() + closingEndTag + node.getName() + openEndTag + newLine);
             }
+            //Writes the basenode in the following format: </classname>
+            bufferedWriter.write(closingEndTag + className + openEndTag + "");
+
         }
     }
 }
