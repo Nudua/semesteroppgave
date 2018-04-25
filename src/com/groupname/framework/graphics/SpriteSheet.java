c90c5a3704532be04c882d0d5e1ae187b115d84a
Tor Ramstad <tor.audun@gmail.com>
2018-04-25 16:56:49 +0200

    SpriteSheet javadoc

--- src/com/groupname/framework/graphics/SpriteSheet.java
+++ src/com/groupname/framework/graphics/SpriteSheet.java
@@ -6,29 +6,64 @@ import javafx.scene.image.Image;
 import java.security.InvalidParameterException;
 import java.util.Objects;
 
+/**
+ * A Spritesheet is based on an image that holds a collection of Sprites.
+ */
 public class SpriteSheet {
-
     private final String name;
     private final Image image;
 
+    /**
+     * Creates a new instance of this class with the specified name and image.
+     * The name parameter cannot be null or empty "".
+     *
+     * @param name the name to use for this Spritesheet.
+     * @param image the image to use for this SpriteSheet.
+     * @throws NullPointerException if either name or image is null.
+     */
     public SpriteSheet(String name, Image image) {
         this.name = Strings.requireNonNullAndNotEmpty(name);
         this.image = Objects.requireNonNull(image, "Sheet cannot be null");
     }
 
+    /**
+     * Gets the width of the Image used by this SpriteSheet.
+     * @return the width of the Image used by this SpriteSheet.
+     */
     public int getWidth() {
         return (int)image.getWidth();
     }
 
+    /**
+     * Gets the height of the Image used by this SpriteSheet.
+     * @return the height of the Image used by this SpriteSheet.
+     */
     public int getHeight() {
         return (int)image.getHeight();
     }
 
+    /**
+     * Gets the name of this SpriteSheet.
+     *
+     * @return the name of this SpriteSheet.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the Image used by this SpriteSheet.
+     * @return the Image used by this SpriteSheet.
+     */
     public Image getImage() {
         return image;
+    }
+
+    @Override
+    public String toString() {
+        return "SpriteSheet{" +
+                "name='" + name + '\'' +
+                ", image=" + image +
+                '}';
     }
 }
