a38d194626d819767917e9dcfe9a80ae2e0503fa
Tor Ramstad <tor.audun@gmail.com>
2018-04-25 16:37:19 +0200

    TaskRunner javadoc

--- src/com/groupname/framework/concurrency/TaskRunner.java
+++ src/com/groupname/framework/concurrency/TaskRunner.java
@@ -12,25 +12,52 @@ import java.util.function.Supplier;
 
 
 /**
- * Convenience class for running short-lived runnables and tasks on a separate thread using the ExecutorService interface.
+ * Convenience class for running methods on a thread using various ExecutorService types.
+ * The default ExecutorService for this class is the CachedThreadPool one.
  *
- * Supports methods for running any methods that implement runnable or
+ * This class uses the fail-fast principle and will throw a NullPointerException if any of the calling parameters are null.
+ *
+ * Users must call stop() before exiting the program to avoid the ExecutorService from stalling.
  */
 public class TaskRunner {
     private final ExecutorService executor;
 
+    /**
+     * Creates a new instance of the TaskRunner with the default ExecutorService (CachedThreadPool)
+     */
     public TaskRunner() {
         executor = Executors.newCachedThreadPool();
     }
 
+    /**
+     * Creates a new instance of this class with the given ExecutorService.
+     *
+     * @param executor the ExecutorService to use internally.
+     */
     public TaskRunner(ExecutorService executor) {
         this.executor = Objects.requireNonNull(executor);
     }
 
+    /**
+     * Returns if the ExecutorService is shutDown or not.
+     *
+     * @return true if the ExecutorService is Shutdown, false if it is still running.
+     */
     public boolean isShutdown() {
         return executor.isShutdown();
     }
 
+    /**
+     * Submit a new Supplier of type T that should be run on a separate thread
+     * using the ExcecutorService.
+     *
+     * The onCompleted Consumer of type T will be invoked on the JavaFX UI Thread
+     * after the Supplier of type T has completed.
+     *
+     * @param action the method to run on a separate thread.
+     * @param onCompleted the method to invoke when the action Supplier has completed.
+     * @param <T> the type of class used for the Supplier and Consumer.
+     */
     public <T> void submit(Supplier<T> action, Consumer<T> onCompleted) {
         Objects.requireNonNull(action);
         Objects.requireNonNull(onCompleted);
@@ -42,11 +69,23 @@ public class TaskRunner {
         });
     }
 
+    /**
+     * A fire and forget method that runs the specified Runnable on a separate thread.
+     *
+     * @param action the runnable to run on a separate thread.
+     */
     public void submit(Runnable action) {
         Objects.requireNonNull(action);
         executor.submit(action);
     }
 
+    /**
+     * Submit a Runnable to be run on a separate thread, with a supplied onCompleted
+     * Runnable that will be called on the JavaFX UI thread when the action Runnable has completed.
+     *
+     * @param action the Runnable to run on a separate thread.
+     * @param onCompleted the Runnable to invoke on the JavaFX thread when the action runnable has completed.
+     */
     public void submit(Runnable action, Runnable onCompleted) {
         Objects.requireNonNull(action);
         Objects.requireNonNull(onCompleted);
@@ -59,12 +98,24 @@ public class TaskRunner {
         });
     }
 
+    /**
+     * Submits a new Task that will be run on the ExecutorService.
+     *
+     * @param task the task to run on the ExecutorService.
+     * @param <V> the generic class parameter that the Task contains.
+     */
     public <V> void submit(Task<V> task) {
         Objects.requireNonNull(task);
 
         executor.submit(task);
     }
 
+    /**
+     * Attempts to shutdown the ExecutorService, it will wait for 2000 milliseconds
+     * before it will force all the currently running actions to quit forcefully.
+     *
+     * @throws InterruptedException if the termination was interrupted.
+     */
     public void stop() throws InterruptedException {
         if(executor.isShutdown()) {
             return;
