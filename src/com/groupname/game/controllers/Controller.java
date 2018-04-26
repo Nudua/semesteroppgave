bcd27a70c8398027a1784c71ef818e689878c709
Håkon Åreskjold <haareskjold@gmail.com>
2018-04-26 15:42:10 +0200

    Javadoc

--- src/com/groupname/game/controllers/Controller.java
+++ src/com/groupname/game/controllers/Controller.java
@@ -4,7 +4,7 @@ import com.groupname.game.core.Game;
 import javafx.scene.canvas.Canvas;
 
 /**
- *
+ * Controller interface that connects the view and the game logic.
  */
 public interface Controller {
     void init(Game game);
