fd034ff8578bafb195dcbe8aeca5d91bc6131a06
Håkon Åreskjold <haareskjold@gmail.com>
2018-04-26 11:59:47 +0200

    CounterTests

--- test/com/groupname/framework/math/CounterTests.java
+++ test/com/groupname/framework/math/CounterTests.java
@@ -1,13 +1,42 @@
 package com.groupname.framework.math;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.security.InvalidParameterException;
 
 public class CounterTests {
 
     @Test(expected = InvalidParameterException.class)
-    public void InvalidWithZeroOrLess() {
+    public void invalidWithZeroOrLess() {
         Counter counter = new Counter(-4);
+    }
+
+    @Test
+    public void stepperSteps() {
+        Counter counter = new Counter(1);
+        assertEquals(counter.getCounter(), 0);
+        counter.step();
+        assertEquals(counter.getCounter(), 1);
+    }
+
+    @Test
+    public void checkIsDone() {
+        Counter counter = new Counter(1);
+        assertFalse(counter.isDone());
+        for(int i = 0; i < 60; i++) {
+            counter.step();
+        }
+        assertTrue(counter.isDone());
+    }
+
+    @Test
+    public void checkReset() {
+        Counter counter = new Counter(1);
+        assertEquals(counter.getCounter(), 0);
+        counter.step();
+        assertEquals(counter.getCounter(), 1);
+        counter.reset();
+        assertEquals(counter.getCounter(), 0);
     }
 }
