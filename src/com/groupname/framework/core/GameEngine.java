e651756a21865310202058619a65f1a3b3796130
Tor Ramstad <tor.audun@gmail.com>
2018-04-26 17:44:34 +0200

    GameEngine Javadoc

--- src/com/groupname/framework/core/GameEngine.java
+++ src/com/groupname/framework/core/GameEngine.java
@@ -2,19 +2,16 @@ package com.groupname.framework.core;
 
 import com.groupname.framework.math.Size;
 import javafx.animation.*;
-import javafx.application.Platform;
-import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+
 import javafx.scene.paint.Color;
-import javafx.util.Duration;
 
-import java.util.Objects;
-import java.util.function.Consumer;
-
-
+/**
+ * The base class used to drive a Game which contains an AnimatingTimer that
+ * updates 60 times per second.
+ *
+ * Subclasses must override update (logic) and draw (used to draw the game world).
+ */
 public abstract class GameEngine {
 
     protected Canvas canvas;
@@ -23,14 +20,17 @@ public abstract class GameEngine {
     protected int height;
 
     private volatile boolean running = false;
-    private volatile boolean paused = false;
 
     protected Color background = Color.CORNFLOWERBLUE;
 
-
     private AnimationTimer animationTimer;
 
-
+    /**
+     * Subclasses must call this method with the specified width and height for the Canvas.
+     *
+     * @param width the width of the game area.
+     * @param height the height of the game area.
+     */
     public GameEngine(int width, int height) {
         this.width = width;
         this.height = height;
@@ -38,45 +38,55 @@ public abstract class GameEngine {
         createAnimationTimer();
     }
 
-
     private void createAnimationTimer() {
         animationTimer = new GameLoopTimer(this::update, this::draw);
     }
 
+    /**
+     * Returns the bounds (Size) of the game area.
+     *
+     * @return the bounds (Size) of the game area.
+     */
     public Size getScreenBounds() {
         return new Size(width, height);
     }
 
-    protected boolean isPaused() {
-        return paused;
-    }
-
-    // not sure yet
-    public void setPaused(boolean paused) {
-        this.paused = paused;
-    }
-
+    /**
+     * Returns whether the game loop is running or not.
+     *
+     * @return true if the game loop is running, false otherwise.
+     */
     public boolean isRunning() {
         return running;
     }
 
+    /**
+     * Starts the game loop if it's not already running.
+     */
     public void start() {
         if(running) {
             return;
         }
 
         running = true;
-
         animationTimer.start();
     }
 
+    /**
+     * Stops the game loop.
+     */
     public void stop() {
         running = false;
         animationTimer.stop();
     }
 
-
+    /**
+     * Implementations must override this to update any game logic, this method will be called 60 times per second.
+     */
     protected abstract void update();
 
+    /**
+     * Implementations must override this to draw the game world, this method will be called 60 times per second.
+     */
     protected abstract void draw();
 }
