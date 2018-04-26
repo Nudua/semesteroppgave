8dd96991163a24d532a6e299a9301dc044fbbcd7
Tor Ramstad <tor.audun@gmail.com>
2018-04-26 14:46:07 +0200

    SpritebatchFX bugs

--- test/com/groupname/framework/graphics/drawing/SpriteBatchFXTests.java
+++ test/com/groupname/framework/graphics/drawing/SpriteBatchFXTests.java
@@ -3,10 +3,23 @@ package com.groupname.framework.graphics.drawing;
 import com.groupname.framework.graphics.SpriteSheet;
 import com.groupname.framework.math.Vector2D;
 import javafx.scene.canvas.Canvas;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import test.util.MockFX;
+
 import static org.junit.Assert.*;
 
 public class SpriteBatchFXTests {
+
+    /*
+    @BeforeClass
+    public static void init() {
+        // For these tests we have to make sure that the Javafx thread is initialized
+        Canvas canvas = new Canvas();
+    }
+    */
+
+    // todo: Why does this suddenly not work?
 
     @Test(expected = NullPointerException.class)
     public void ConstructorParameterGraphicsContextCannotBeNull() {
