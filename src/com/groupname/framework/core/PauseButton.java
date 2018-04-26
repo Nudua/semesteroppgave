a821c9e269a9bfd92ae156983b0b5be1cc7cde2c
Håkon Åreskjold <haareskjold@gmail.com>
2018-04-26 17:11:53 +0200

    BoundsChecker tests + javadoc

--- src/com/groupname/framework/core/PauseButton.java
+++ src/com/groupname/framework/core/PauseButton.java
@@ -4,9 +4,29 @@ package com.groupname.framework.core;
  * Represents the different actions a PauseMenu can execute.
  */
 public enum PauseButton {
+
+    /**
+     * Resumes the game.
+     */
     Resume,
+
+    /**
+     * Saves the game.
+     */
     Save,
+
+    /**
+     * Restart the level.
+     */
     RestartLevel,
+
+    /**
+     * Restart the game.
+     */
     RestartGame,
+
+    /**
+     * Take you to main menu.
+     */
     MainMenu
 }
