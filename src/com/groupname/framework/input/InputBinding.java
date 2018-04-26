f1a3327233625a4f67ea1d3d0b40a0a6794cb059
Tor Ramstad <tor.audun@gmail.com>
2018-04-26 14:41:50 +0200

    InputBinding javadoc

--- src/com/groupname/framework/input/InputBinding.java
+++ src/com/groupname/framework/input/InputBinding.java
@@ -1,5 +1,6 @@
 package com.groupname.framework.input;
 
+import com.groupname.framework.util.EmptyStringException;
 import com.groupname.framework.util.Strings;
 
 import java.security.InvalidParameterException;
@@ -16,12 +17,28 @@ public class InputBinding {
 
     private Set<String> bindings;
 
+    /**
+     * Creates a new instance of this class with the specified name for this inputBinding,
+     * with no initial bindings to bind to.
+     *
+     * @param name the name to use for this inputbinding.
+     * @throws NullPointerException if the name is null.
+     * @throws EmptyStringException if the name is empty (equal to the String "")
+     */
     public InputBinding(String name) {
         this.name = Strings.requireNonNullAndNotEmpty(name);
 
         this.bindings = new HashSet<>();
     }
 
+    /**
+     * Creates a new instance of this class with the specified name and inputbindings.
+     *
+     * @param name the name to use for this inputbinding.
+     * @param bindings the bindings to add to this inputbinding.
+     * @throws NullPointerException if the name is null, or the bindings are null or empty.
+     * @throws EmptyStringException if the name is empty (equal to the String "")
+     */
     public InputBinding(String name, String ... bindings) {
         this.name = Strings.requireNonNullAndNotEmpty(name);
 
@@ -34,18 +51,42 @@ public class InputBinding {
         this.bindings.addAll(Arrays.asList(bindings));
     }
 
+    /**
+     * Returns the name of this inputbinding.
+     *
+     * @return the name of this inputbinding.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Adds a new binding to this inputbinding.
+     * @param button the String binding to add.
+     */
     public void addBinding(String button) {
         Strings.requireNonNullAndNotEmpty(button);
         bindings.add(button);
     }
 
+    /**
+     * Returns the bindings associated by this instance.
+     *
+     * @return the bindings associated by this instance.
+     */
     public Set<String> getBindings() {
         return bindings;
     }
 
-
+    /**
+     * Returns the String representation of this object, the name and all of the bindings used for this instance.
+     * @return the name and all of the bindings used for this instance.
+     */
+    @Override
+    public String toString() {
+        return "InputBinding{" +
+                "name='" + name + '\'' +
+                ", bindings=" + bindings +
+                '}';
+    }
 }
