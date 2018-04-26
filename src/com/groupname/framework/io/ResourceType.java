dff7a4e6313fc63e1f1a8abd4045a11ca3c74d74
Håkon Åreskjold <haareskjold@gmail.com>
2018-04-26 16:50:15 +0200

    ResourceType javadoc

--- src/com/groupname/framework/io/ResourceType.java
+++ src/com/groupname/framework/io/ResourceType.java
@@ -1,10 +1,36 @@
 package com.groupname.framework.io;
 
+/**
+ * Enum list for our supported resource types.
+ */
 public enum ResourceType {
+    /**
+     * An image file with multiple sprites.
+     */
     SpriteSheet,
+
+    /**
+     * An image file that represents a sprite.
+     */
     Sprite,
+
+    /**
+     * An audio file that plays in the background.
+     */
     Music,
+
+    /**
+     * An audio file that get triggered by specific actions.
+     */
     SoundEffect,
+
+    /**
+     * An image file that represent a background.
+     */
     Background,
+
+    /**
+     * An level file that contain a specific level.
+     */
     Level
 }
