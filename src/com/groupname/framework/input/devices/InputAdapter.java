a1f6ea23ca9e750d9f521368635d898b55f96bbc
Håkon Åreskjold <haareskjold@gmail.com>
2018-04-26 14:58:41 +0200

    InputAdapter Javadoc

--- src/com/groupname/framework/input/devices/InputAdapter.java
+++ src/com/groupname/framework/input/devices/InputAdapter.java
@@ -2,12 +2,27 @@ package com.groupname.framework.input.devices;
 
 import java.util.Set;
 
-// https://en.wikipedia.org/wiki/Adapter_pattern
-// Converge different input types into a simple unified interface
+
+/**
+ * Interface that collects the different types of inputs.
+ * The implementations must implements all the methods.
+ */
+
 public interface InputAdapter {
 
     boolean isEnabled();
+
+    /**
+     * The implementations must use this method to enable or disable the InputAdapter.
+     *
+     * @param enabled true or false.
+     */
     void setEnabled(boolean enabled);
 
+    /**
+     * The implementations must use this method to update the specified set with the internal currently pressed input.
+     *
+     * @param digitalInput the collection to add the currently pressed input.
+     */
     void update(Set<String> digitalInput);
 }
