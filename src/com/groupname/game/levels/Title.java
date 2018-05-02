1484d538326271db0a7ad56e63e16465f68cd8ee
Håkon Åreskjold <haareskjold@gmail.com>
2018-05-02 19:28:45 +0200

    Title title

--- src/com/groupname/game/levels/Title.java
+++ src/com/groupname/game/levels/Title.java
@@ -1,13 +1,22 @@
 package com.groupname.game.levels;
 
-import com.groupname.framework.core.GameEngine;
-import com.groupname.framework.input.InputManager;
+import com.groupname.framework.core.GameObject;
+import com.groupname.framework.graphics.Sprite;
+import com.groupname.framework.io.Content;
+import com.groupname.framework.io.ResourceType;
+import com.groupname.framework.math.Vector2D;
 import com.groupname.game.core.Game;
+import com.groupname.game.entities.EnemySpriteType;
+import com.groupname.game.entities.SpriteFactory;
+import com.groupname.game.entities.enemies.GuardEnemy;
 import com.groupname.game.input.PlayerInputDefinitions;
 import com.groupname.game.levels.core.LevelBase;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+
+import java.nio.file.Path;
+import java.util.Arrays;
 
 public class Title extends LevelBase {
 
@@ -15,8 +24,9 @@ public class Title extends LevelBase {
 
     public Title(Game parent, GraphicsContext graphicsContext) {
         super(parent, graphicsContext);
+        createObject();
 
-        backgroundColor = Color.BLACK;
+        backgroundColor = Color.WHITE;
     }
 
     @Override
@@ -29,26 +39,45 @@ public class Title extends LevelBase {
 
     }
 
+    private void createObject() {
+        SpriteFactory spriteFactory = new SpriteFactory();
+        Sprite sprite = spriteFactory.createEnemy(EnemySpriteType.PinkFish);
+        GuardEnemy enemy1 = new GuardEnemy(sprite, new Vector2D(400,50));
+
+        Sprite sprite2 = spriteFactory.createEnemy(EnemySpriteType.BlueBlob);
+        GuardEnemy enemy2 = new GuardEnemy(sprite2, new Vector2D(700,500));
+
+        gameObjects.addAll(Arrays.asList(enemy1, enemy2));
+    }
+
     @Override
     public void update() {
         //inputManager.update();
-
         if(inputManager.isDown(PlayerInputDefinitions.DOWN)) {
             graphicsContext.setFill(Color.GREEN);
             graphicsContext.setFont(Font.font(60));
             graphicsContext.fillText("PRESSING DOWN", 10, 500);
         }
-    }
+
+        for(GameObject gameObject : gameObjects) {
+            gameObject.update();
+        }
+
+        }
 
     @Override
     public void draw() {
         clearScreen();
+        for(GameObject gameObject : gameObjects) {
+            gameObject.draw(spriteBatch);
+        }
+        graphicsContext.drawImage(Content.loadImage("hill.png", ResourceType.Background), 100,420);
 
-        graphicsContext.setFill(Color.GREEN);
+       /* graphicsContext.setFill(Color.GREEN);
         graphicsContext.setFont(Font.font(60));
         graphicsContext.fillText("We're on the title screen!", 10, 50);
         graphicsContext.setFont(Font.font(30));
-        graphicsContext.fillText("(add some freaken animation or something here)", 10, 85);
+        graphicsContext.fillText("(add some freaken animation or something here)", 10, 85);*/
 
     }
 }
