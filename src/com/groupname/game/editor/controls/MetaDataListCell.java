7150c88cd580eecec2112c6098b3f62a94841d3a
Tor Ramstad <tor.audun@gmail.com>
2018-05-02 17:10:51 +0200

    MetaDataListCell javadoc and updates

--- src/com/groupname/game/editor/controls/MetaDataListCell.java
+++ src/com/groupname/game/editor/controls/MetaDataListCell.java
@@ -3,12 +3,16 @@ package com.groupname.game.editor.controls;
 import com.groupname.game.editor.metadata.ObjectMetaData;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.control.OverrunStyle;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 
+/**
+ * This class represents a custom ListCell for the ObjectMetaData class.
+ */
 public class MetaDataListCell extends ListCell<ObjectMetaData> {
     @Override
     protected void updateItem(ObjectMetaData item, boolean empty) {
@@ -16,7 +20,8 @@ public class MetaDataListCell extends ListCell<ObjectMetaData> {
 
         if(item != null) {
             Label label = new Label(item.getName());
-            label.setPrefSize(80,80);
+            label.setWrapText(true);
+            label.setPrefSize(100,100);
             label.setBorder(new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.DASHED, CornerRadii.EMPTY, BorderStroke.MEDIUM)));
             setGraphic(label);
         }
