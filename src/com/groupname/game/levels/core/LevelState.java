3563bf0b8ddb39f726f52d8a1f977421163f5ae4
Håkon Åreskjold <haareskjold@gmail.com>
2018-05-04 14:39:30 +0200

    LevelState - javadoc

--- src/com/groupname/game/levels/core/LevelState.java
+++ src/com/groupname/game/levels/core/LevelState.java
@@ -1,8 +1,27 @@
 package com.groupname.game.levels.core;
 
+/**
+ * Enum list representing the states of the level.
+ */
 public enum LevelState {
+
+    /**
+     * Loading level.
+     */
     LOADING,
+
+    /**
+     * Playing level.
+     */
     PLAYING,
+
+    /**
+     * Level completed.
+     */
     COMPLETED,
+
+    /**
+     * Game over.
+     */
     GAME_OVER
 }
